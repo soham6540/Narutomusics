b6fd552af277a16d44abe7ac05d5b204bd824c79
soham6540 <savitafarkade6540@gmail.com>
2024-08-02 10:41:21 +0530

    Update __init__.py

--- DAXXMUSIC/__init__.py
+++ DAXXMUSIC/__init__.py
@@ -1,14 +1,12 @@
 from DAXXMUSIC.core.bot import DAXX
 from DAXXMUSIC.core.dir import dirr
-from DAXXMUSIC.core.git import git
 from DAXXMUSIC.core.userbot import Userbot
-from DAXXMUSIC.misc import dbb, heroku
+from DAXXMUSIC.misc import dbb
 
 from SafoneAPI import SafoneAPI
 from .logging import LOGGER
 
 dirr()
-git()
 dbb()
 heroku()
 
